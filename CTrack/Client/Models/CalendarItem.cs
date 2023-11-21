1faaddf95402faf244ae5c4b7cd0fb68b841f72c
Maximiliano Zulli <if19b183@dev.azure.com>
2023-11-21 21:08:19 +0100

    Added Basic Calendar Component - displays month

---
+++ CTrack/Client/Models/CalendarItem.cs
@@ -0,0 +1,8 @@
+﻿namespace CTrack.Client.Models
+{
+    public class CalendarItem
+    {
+        public DateTime Date { get; set; }
+        public string Name { get; set; } = "";
+    }
+}
---
+++ CTrack/Shared/Entities/TagEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CTrack.Shared.Models.Entities
+{
+    public class TagEntity : Entity
+    {
+        
+    }
+}
